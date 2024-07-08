5ee5dd0c4cc5dffd7fb40dbf5d25a958d81b8429
Yosbel <yosbelmc22@gmail.com>
2024-07-08 15:20:28 -0400

    ordered the messages according pub_date

--- mychatapp/views.py
+++ mychatapp/views.py
@@ -3,6 +3,7 @@ from .models import Profile, ChatMessage
 from .forms import MessageForm
 from django.http import JsonResponse
 import json
+from itertools import chain
 
 # Create your views here.
 def user_details(request, id):
@@ -18,11 +19,10 @@ def chatting(request, id):
     friends = Profile.objects.exclude(id=id)
     
     last_msg = []
-    nnm = "No messages"
     for f in friends:
-        messages = ChatMessage.objects.filter(receiver=user, sender=f)
+        messages = ChatMessage.objects.filter(receiver=user, sender=f).order_by('pub_date')
         if len(messages) == 0:
-            last_msg.append({'friend_id': f.id, 'mensaje': nnm})
+            last_msg.append({'friend_id': f.id, 'mensaje': "No messages"})
         else:
             ultimo = len(messages) - 1
             last_msg.append({'friend_id': f.id, 'mensaje': messages[ultimo]})
@@ -43,11 +43,16 @@ def unreadMessagesCount(request, user_id):
     return JsonResponse(unread_counts, safe=False)
 
 
+
+
 def chat_screen(request, id, r_id):
     user = Profile.objects.get(id=id)
     user_receiver = Profile.objects.get(id = r_id)
-    chat_send = ChatMessage.objects.filter(sender_id = id, receiver_id = r_id)
-    chat_receive = ChatMessage.objects.filter(sender_id = r_id, receiver_id = id)
+    
+    chat_send = ChatMessage.objects.filter(sender_id = id, receiver_id = r_id).order_by('pub_date')
+    chat_receive = ChatMessage.objects.filter(sender_id = r_id, receiver_id = id).order_by('pub_date')
+    combined_chats = sorted(chain(chat_send, chat_receive), key=lambda message: message.pub_date)
+    
     mensaje_sent = []
     mensaje_received = []
     
@@ -67,7 +72,15 @@ def chat_screen(request, id, r_id):
         return redirect('actualchat', id, r_id)
     else:
         form = MessageForm()
-    context ={'user': user, 'form': form, 'frnd': frnd, 'mensaje_sent': mensaje_sent, 'mensaje_received': mensaje_received, 'chat_send': chat_send, 'chat_receive': chat_receive, 'friend':friend}
+    context ={'user': user, 
+              'form': form, 
+              'frnd': frnd, 
+              'mensaje_sent': mensaje_sent, 
+              'mensaje_received': mensaje_received, 
+              'chat_send': chat_send, 
+              'chat_receive': chat_receive, 
+              'friend':friend,
+              'combined_chats': combined_chats,}
     return render(request, 'actualchat.html', context)
 
 
